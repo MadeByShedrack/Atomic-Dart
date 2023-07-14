b320b68d293f5d603ed636b13645f66a46303131
= <=>
2023-07-14 23:35:43 +0100

    .

--- functions/example17.dart
+++ functions/example17.dart
@@ -1,6 +1,13 @@
 void main() {
   final foo = doSomething(10, 20)();
   print(foo);
+  print("");
+  String answer = greetUser(user: "John");
+  print("User Greet -> $answer");
 }
 
 int Function() doSomething(int lhs, int rhs) => () => lhs + rhs;
+
+String greetUser({required String user}) {
+  return "The User Greetings Is -> $user";
+}
